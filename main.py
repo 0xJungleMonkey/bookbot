15b10c05183a5c9f1ea2e0acea7fe322fc37f1a7
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-04-22 17:34:58 -0400

    readtextfile

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, world")
+import string
+
+
+def main():
+    book_path = "books/Franskenstein.txt"
+    contents = get_file_contents(book_path)
+    single_words = split_words(contents)
+    count_letters(contents)
+    # print(contents)
+    # print(single_words)
+
+
+def get_file_contents(book_path):
+    with open(book_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def split_words(contents):
+    words = contents.split()
+    return words
+
+
+def count_letters(contents):
+    account_of_letters = None
+    letters = string.ascii_lowercase[:]
+    print(letters)
+
+
+main()
